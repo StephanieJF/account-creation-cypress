591f201f71fb4a9f791f1f231f5ccd670ef6117e
Stephanie Fischler <sjfischler@gmail.com>
2023-06-20 18:35:19 -0400

    Add unhappy path to UI tests

--- cypress/e2e/UI-Testing/register.js
+++ cypress/e2e/UI-Testing/register.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 describe('Register for a new account via the UI', () => {
-  before(function () {
+  beforeEach(function () {
     //generate unique data and write it into the fixture file
     cy.createUniqueUser();
 
@@ -20,5 +20,29 @@ describe('Register for a new account via the UI', () => {
     //validate successful registration via UI elements
     cy.get('app-article-list').should('contain.text', 'No articles are here');
     cy.get('.container > .nav').should('contain.text', this.user.username);
+  });
+
+  it.only('Cannot register with missing or non-unique username and email', function () {
+    cy.request({
+      method: 'POST',
+      url: 'https://api.realworld.io/api/users',
+      body: { user: this.user },
+    });
+    cy.getRegistrationPage();
+    //submit button remains inactive until required fields are filled
+    cy.get('button[type="submit"]').should('be.disabled');
+
+    cy.get('input[placeholder="Username"]').type(this.user.username);
+    cy.get('button[type="submit"]').should('be.disabled'); //still disabled
+
+    cy.get('input[placeholder="Email"]').type(this.user.email);
+    cy.get('button[type="submit"]').should('be.disabled'); //still disabled
+
+    cy.get('input[placeholder="Password"]').type(this.user.password);
+    cy.get('button[type="submit"]').should('be.enabled'); //now enabled
+
+    //non-unique username and email should throw errors
+    cy.get('button[type="submit"]').click();
+    cy.get('.error-messages').find('li').should('have.length', 2);
   });
 });
