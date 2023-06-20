f4ccc8988b4884bf4a23a83fc678873c1d29c6ef
Stephanie Fischler <sjfischler@gmail.com>
2023-06-20 16:50:15 -0400

    Transfer repeatable code to custom commands

--- cypress/e2e/UI-Testing/register.js
+++ cypress/e2e/UI-Testing/register.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('Register for a new account via the UI', () => {
   before(function () {
-    //custom command to generate unique data and write it into the fixture file
+    //generates unique data and write it into the fixture file
     cy.createUniqueUser();
 
     //make fixture data available
@@ -11,21 +11,13 @@ describe('Register for a new account via the UI', () => {
   });
 
   it('Successfully registers a new account', function () {
-    cy.visit('');
-    cy.get('.nav-link').contains('Sign up').click(); //click signup button
-
-    // assert sign up button took user to sign up page
-    cy.url().should('contain', '/register');
-    cy.get('h1').should('have.text', 'Sign up');
-
-    cy.get('input[placeholder="Username"]').type(this.user.username);
-    cy.get('input[placeholder="Email"]').type(this.user.email);
-    cy.get('input[placeholder="Password"]').type(this.user.password);
+    cy.getRegistrationPage();
+    cy.fillRegistrationForm(this.user);
 
     cy.get('button[type="submit"]').click(); //submit the form
 
-    //validate registration was successful via UI elements
-    cy.get('app-article-list').should('contain.text', 'No articles are here'); //new user has nothing in their posts
-    cy.get('.container > .nav').should('contain.text', this.user.username); //username displayed when logged in
+    //validate successful registration via UI elements
+    cy.get('app-article-list').should('contain.text', 'No articles are here');
+    cy.get('.container > .nav').should('contain.text', this.user.username);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,3 +4,18 @@ Cypress.Commands.add('createUniqueUser', () => {
   let uniqueEmail = uniqueUsername + '@GoodDogs.com';
   cy.writeFile('cypress/fixtures/userInfo.json', { username: `${uniqueUsername}`, email: `${uniqueEmail}`, password: 'ilovetreats456' });
 });
+
+Cypress.Commands.add('getRegistrationPage', () => {
+  cy.visit('');
+  cy.get('.nav-link').contains('Sign up').click(); //click signup button
+
+  // assert sign up button took user to sign up page
+  cy.url().should('contain', '/register');
+  cy.get('h1').should('have.text', 'Sign up');
+});
+
+Cypress.Commands.add('fillRegistrationForm', (user) => {
+  cy.get('input[placeholder="Username"]').type(user.username);
+  cy.get('input[placeholder="Email"]').type(user.email);
+  cy.get('input[placeholder="Password"]').type(user.password);
+});
